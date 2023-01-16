4492b7354533d10eea305433c5f0b046bedc098e
Nodonunderscore <aaro.p.salminen@gmail.com>
2023-01-16 19:12:53 +0200

    added a python clicker file, and wrote some code

---
+++ clicker.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+win = tk.Tk()
+win.title("basic clicker game")
+a = 0
+
+def press():
+    a += 1
+    label.config(text=f"Times clicked: {a}")
+
+label = tk.Label(master=win, text="Times clicked: none")
+button = tk.Button(master=win, command=press)
