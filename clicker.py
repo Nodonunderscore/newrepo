67c97b5fc36438460763b29d557d281612358972
Nodonunderscore <aaro.p.salminen@gmail.com>
2023-01-17 16:55:17 +0200

    added QUITE a few of fixes

--- clicker.py
+++ clicker.py
@@ -1,13 +1,18 @@
 import tkinter as tk
 win = tk.Tk()
 win.title("basic clicker game")
+win.geometry("500x300")
 a = 0
 
 def press():
+    global a
     a += 1
     label.config(text=f"Times clicked: {a}")
 
-label = tk.Label(master=win, text="Times clicked: none")
-button = tk.Button(master=win, command=press)
+label = tk.Label(master=win, text="Times clicked: none", width=400, height=50)
+button = tk.Button(master=win, command=press, width=400, height=50)
+
+label.grid(row=0, column=0)
+button.grid(row=1, column=0)
 
 win.mainloop()
