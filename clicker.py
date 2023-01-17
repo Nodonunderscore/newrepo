244ad99c8fab237152936a642ce38d2fd0ef6a65
Nodonunderscore <aaro.p.salminen@gmail.com>
2023-01-17 19:45:58 +0200

    tried to fix button placement, emphasis on "tried"

--- clicker.py
+++ clicker.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 win = tk.Tk()
 win.title("basic clicker game")
-win.geometry("500x300")
+win.geometry("290x300")
 a = 0
 
 def press():
@@ -9,10 +9,10 @@ def press():
     a += 1
     label.config(text=f"Times clicked: {a}")
 
-label = tk.Label(master=win, text="Times clicked: none", width=400, height=50)
-button = tk.Button(master=win, command=press, width=400, height=50)
+label = tk.Label(master=win, text="Times clicked: none", width=150, height=20)
+button = tk.Button(master=win, command=press, width=150, height=200)
 
-label.grid(row=0, column=0)
-button.grid(row=1, column=0)
+label.pack()
+button.pack()
 
 win.mainloop()
